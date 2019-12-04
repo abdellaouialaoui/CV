02f116f96144e7026f28b48029935896e3870d47
El Arbi ABDELLAOUI ALAOUI <elarbi.abdellaoui@eigsinet.org>
2019-12-05 00:06:24 +0100

    Footer & Work in Progress

---
+++ js/app.js
@@ -0,0 +1,85 @@
+
+//we can set animation delay as following in ms (default 1000)
+ProgressBar.singleStepAnimation = 1000;
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper' // created this optional parameter for container name (otherwise default container created)
+);
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper2' // created this optional parameter for container name (otherwise default container created)
+);
+
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper3' // created this optional parameter for container name (otherwise default container created)
+);
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper4' // created this optional parameter for container name (otherwise default container created)
+);
+
+
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper5' // created this optional parameter for container name (otherwise default container created)
+);
+
+
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper6' // created this optional parameter for container name (otherwise default container created)
+);
+
+
+ProgressBar.init(
+  [ 'Outline',
+    'Approach',
+    'Results',
+    'Edit',
+    'Proofread and submit'
+  ],
+  'Edit',
+  'progress-bar-wrapper7' // created this optional parameter for container name (otherwise default container created)
+);
